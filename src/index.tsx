54b55a36089c59965c6bf771e62746502dbbaf49
dn004 <d.ntwari@alustudent.com>
2025-05-01 21:44:47 +0200

    new updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import './index.css';
-import React from "react";
-import { render } from "react-dom";
-import { App } from "./App";
-render(<App />, document.getElementById("root"));
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+}
